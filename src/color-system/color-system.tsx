5cf5c5677630a9d98e59b6c1077f79884db808f2
gitworkflows <118260833+gitworkflows@users.noreply.github.com>
2024-05-08 20:05:18 +0600

    Update color-system.tsx

--- src/color-system/color-system.tsx
+++ src/color-system/color-system.tsx
@@ -12,25 +12,31 @@ import { Color, PaletteProps } from "./types";
  * Output: [{ label: 'blue10', value: 'var(--blue10)' }]
  */
 
-// Transform colorList from an object to an array of objects
-const transformedColorList = Object.keys(colorList).map((colorName) => ({
-  label: colorName.replace("--", ""),
-  value: colorList[colorName],
-}));
-
 // Get the background and text color values of each palette item
 const getPaletteItemStyle = (color: Color) => {
   const itemTextColor = color.label.substring(color.label.length - 2);
+  const textColor = parseInt(itemTextColor, 10) >= 50 ? "#ffffff" : "#0a0a23";
 
   return {
     backgroundColor: color.value,
-    color: parseInt(itemTextColor, 10) >= 50 ? "#ffffff" : "#0a0a23",
+    color: textColor,
   };
 };
 
 // Get palette colors by name
-const getPaletteByColorName = (name: string) =>
-  transformedColorList.filter((color) => color.label.includes(name));
+const getPaletteByColorName = (name: string): Color[] => {
+  const filteredColors: Color[] = [];
+
+  for (const colorName in colorList) {
+    if (colorName.includes(name)) {
+      const label = colorName.replace("--", "");
+      const value = colorList[colorName];
+      filteredColors.push({ label, value });
+    }
+  }
+
+  return filteredColors;
+};
 
 // Palette component
 const Palette: React.FC<PaletteProps> = ({ colors }) => {
@@ -51,17 +57,9 @@ const Palette: React.FC<PaletteProps> = ({ colors }) => {
 
 // AllPalettes component
 export const AllPalettes: React.FC = () => {
-  // Attempt to get palette colors, handle error if colors.A is undefined
-  let paletteColors = [];
-  try {
-    paletteColors = getPaletteByColorName("gray");
-  } catch (error) {
-    console.error("Error getting palette colors:", error);
-  }
-
   return (
     <>
-      <Palette colors={paletteColors} />
+      <Palette colors={getPaletteByColorName("gray")} />
       <Palette colors={getPaletteByColorName("purple")} />
       <Palette colors={getPaletteByColorName("yellow")} />
       <Palette colors={getPaletteByColorName("blue")} />
